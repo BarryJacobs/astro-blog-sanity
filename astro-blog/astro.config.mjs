de91f5275c1b2aba114ebe3f0b28a0466d3548db
Barry Jacobs <barry.jacobs@mechenet.com>
2023-09-24 05:40:40 +0100

    Netlify fix

--- astro-blog/astro.config.mjs
+++ astro-blog/astro.config.mjs
@@ -7,6 +7,11 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: `${import.meta.env.VITE_WEBSITE_URL}`,
+  vite: {
+    ssr: {
+      noExternal: ["path-to-regexp"],
+    },
+  },
   server: {
     port: 8080,
     host: true,
