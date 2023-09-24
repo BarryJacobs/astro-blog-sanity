34d16fe5bc3eaa75a1d87558965516c856353bf2
Barry Jacobs <barry.jacobs@mechenet.com>
2023-09-24 12:56:44 +0100

    Removed slugify

--- astro-blog/src/scripts/utils.ts
+++ astro-blog/src/scripts/utils.ts
@@ -3,16 +3,6 @@ export const formatDate = (date: Date) =>
     timeZone: "UTC",
   });
 
-export const slugify = (text: string) =>
-  text
-    .toString()
-    .toLowerCase()
-    .replace(/\s+/g, "-")
-    .replace(/[^\w-]+/g, "")
-    .replace(/--+/g, "-")
-    .replace(/^-+/, "")
-    .replace(/-+$/, "");
-
 export const isIntegerString = (
   value: string | undefined,
 ): number | undefined => {
